bf804a52664445c55f015eb6421478b982564d2c
espoupou <espoirativon@gmail.com>
2023-12-04 14:26:40 -0800

    update 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 #!/usr/bin/pup
 # Install flask 2.1.0
 
-exec { 'puppet-lint':
-  command => 'pip3 install flask==2.1.0',
+exec { 'flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.2.2',
 }
-
-#package {'flask':
-#  ensure   => '2.1.0',
-#  provider => 'pip3'
-#}
