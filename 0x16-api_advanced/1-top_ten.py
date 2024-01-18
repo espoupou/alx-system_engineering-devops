f07a3f0e4492e53300855abe81f0b68dd878d327
espoupou <espoirativon@gmail.com>
2024-01-17 18:20:17 -0800

    all

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""prints the top ten hot posts of a subreddit
+"""
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """ Queries to Reddit API """
+    user_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
+        return
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
